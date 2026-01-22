d9dc4e23cabcb042ae64609b3fd063c633e85cef
PawelSzoltysek93 <pawel.szoltysek.dev@gmail.com>
2026-01-22 12:11:56 +0100

    add POST /api/products with basic validation

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
 import { ProductModel } from "../models/product.model";
 
+// GET ALL PRODUCTS
 export const getAllProducts = async (_req: Request, res: Response) => {
   try {
     const products = await ProductModel.find();
     res.json(products);
     res.status(200);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to fetch products" });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: error.messagge ?? "Failed to fetch products" });
+  }
+};
+
+//POST PRODUCT
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, price, stock } = req.body;
+    if (!name || price < 0 || stock < 0) {
+      return res.status(400).json({ Message: "Missing required fields" });
+    }
+    const product = await ProductModel.create(req.body);
+    res.status(201).json(product);
+  } catch (error: any) {
+    res
+      .status(400)
+      .json({ message: error.message ?? "Failed to create product" });
   }
 };
--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controller";
+import {
+  getAllProducts,
+  createProduct,
+} from "../controllers/product.controller";
 
 const router = Router();
 
 router.get("/", getAllProducts);
+router.post("/", createProduct);
 
 export default router;
